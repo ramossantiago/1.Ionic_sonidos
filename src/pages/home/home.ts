877f3c853981d303d6eaff15c781cc96e73db61c
wertrtert <sfsdfsfsdqsdfsdfsd@sfdsdfsdf.com>
2018-01-23 17:58:10 -0500

    Lista con imagenes sin reproduccion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {ANIMALES} from '../../data/data.animales';
+import {Animal} from '../../interfaces/animal.interfaces'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales:Animal[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
+    this.animales = ANIMALES.splice(0);
+  }
+
+  reproducir(animal: Animal){
+
+    console.log(animal);
+    let audio = new Audio();
+    audio.src = animal.audio;
+    audio.load;
+    audio.play;
+
+    animal.reproduciendo = true;
+    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
 
   }
+
 
 }
