4eb063e862b0124cbb67eb69a6bb7c34d69fda5f
wertrtert <sfsdfsfsdqsdfsdfsd@sfdsdfsdf.com>
2018-01-24 14:57:21 -0500

    terminado app sonidos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {ANIMALES} from '../../data/data.animales';
 import {Animal} from '../../interfaces/animal.interfaces'
+import { Refresher, reorderArray } from "ionic-angular";
+
 
 @Component({
   selector: 'page-home',
@@ -8,23 +10,58 @@ import {Animal} from '../../interfaces/animal.interfaces'
 })
 export class HomePage {
   animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando: boolean = false;
 
   constructor() {
-    this.animales = ANIMALES.splice(0);
+    this.animales = ANIMALES.slice(0);
   }
 
   reproducir(animal: Animal){
-
     console.log(animal);
-    let audio = new Audio();
-    audio.src = animal.audio;
-    audio.load;
-    audio.play;
+    this.pauseAudio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
 
     animal.reproduciendo = true;
-    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
+    this.audioTiempo = setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
 
   }
 
+  private pauseAudio(animalSeleccionado: Animal) {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSeleccionado.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrarAnimal(index:number){
+    this.animales.splice(index, 1);
+  }
+
+  recargarAnimales(refresher:Refresher){
+    setTimeout(() => {
+      this.animales = ANIMALES.slice(0);
+      refresher.complete();
+    }, 500);
+  }
+
+  reordenarAnimales(eventIndex:any){
+    this.animales = reorderArray(this.animales, eventIndex);
+  }
 
 }
